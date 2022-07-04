03983bab5001c2f946220687d50793394b5c0e35
Yuri-debug <yurai.pustovoy@gmail.com>
2022-07-04 12:53:18 +0300

    init

--- src/pages/Home/Weather/WeatherDispatcher.js
+++ src/pages/Home/Weather/WeatherDispatcher.js
@@ -13,25 +13,26 @@ function WeatherDispatcher() {
   const [uv, setUv] = useState();
 
   useEffect(() => {
-    fetch(
+    const request = fetch(
       "http://api.weatherapi.com/v1/current.json?key=95b27957d7b24ef5872124347221006&q=Sumy&aqi=yes"
     )
-      .then((res) => res.json())
-      .then((res) => {
-        setTemp(res.current.temp_c);
-        setHumid(res.current.humidity);
-        setImg(res.current.condition.icon);
-        setLoc(
-          res.location.name +
-            ", " +
-            res.location.region +
-            ", " +
-            res.location.country
-        );
-        setWind(res.current.wind_kph);
-        setPress(res.current.pressure_mb);
-        setUv(res.current.uv);
-      });
+    const data = await request.json()
+      // .then((res) => res.json())
+      // .then((res) => {
+      //   setTemp(res.current.temp_c);
+      //   setHumid(res.current.humidity);
+      //   setImg(res.current.condition.icon);
+      //   setLoc(
+      //     res.location.name +
+      //       ", " +
+      //       res.location.region +
+      //       ", " +
+      //       res.location.country
+      //   );
+      //   setWind(res.current.wind_kph);
+      //   setPress(res.current.pressure_mb);
+      //   setUv(res.current.uv);
+      // });
   });
 
   return [location, temp, img, humid, wind, pressure, uv];
