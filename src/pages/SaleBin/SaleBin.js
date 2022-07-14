55d73deb576492677d5072f7418d3fdf8d928167
Yuri-debug <yurai.pustovoy@gmail.com>
2022-07-14 11:11:56 +0300

    refactored code

--- src/pages/SaleBin/SaleBin.js
+++ src/pages/SaleBin/SaleBin.js
@@ -1,25 +1,9 @@
 import { Content, Footer, FooterBottom, Header } from "../../components";
 
-import { useEffect, useState } from "react";
-
 function SaleBin() {
-  const [sum, setSum] = useState(Number(localStorage.getItem("sum")) || 0);
-  const [isClicked, setClick] = useState(false);
-
-  useEffect(() => {
-    if (isClicked) {
-      setSum(0);
-    }
-    setClick(false);
-  }, [isClicked]);
-
-  useEffect(() => {
-    localStorage.setItem("sum", sum);
-  }, [sum]);
-
   return (
     <div>
-      <Header sum={sum} setClick={setClick} />
+      <Header />
       <Content />
       <Footer />
       <FooterBottom />
